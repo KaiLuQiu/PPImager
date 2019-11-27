64df702b1cdb3f4914c8a6c3788c8482f6083c77
邱开禄 <944624367@qq.com>
2019-11-27 22:04:20 +0800

    [feature] Initialization project
    1. Initialization project

---
+++ ImageProcessing/ImageProcessing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageProcessing
+//
+//  Created by 邱开禄 on 2019/11/27.
+//  Copyright © 2019 邱开禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageProcessing/ImageProcessingTests/ImageProcessingTests.m
@@ -0,0 +1,37 @@
+//
+//  ImageProcessingTests.m
+//  ImageProcessingTests
+//
+//  Created by 邱开禄 on 2019/11/27.
+//  Copyright © 2019 邱开禄. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImageProcessingTests : XCTestCase
+
+@end
+
+@implementation ImageProcessingTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
