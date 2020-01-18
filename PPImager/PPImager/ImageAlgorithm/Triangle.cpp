b0570091b77482b2c3e337b4ff2bc1d5b0f27dfd
邱开禄 <944624367@qq.com>
2020-01-18 21:05:49 +0800

    [feature] Add triangulation algorithm, not implemented
    1.Add triangulation algorithm, not implemented

---
+++ PPImager/PPImager/ImageAlgorithm/Triangle.cpp
@@ -0,0 +1,17 @@
+//
+//  Triangle.h
+//  PPImager
+//
+//  Created by 邱开禄 on 2020/01/18.
+//  Copyright © 2019 邱开禄. All rights reserved.
+//
+
+#include "Triangle.h"
+#include "math.h"
+#include "string.h"
+#include "stdlib.h"
+NS_IMAGE_BEGIN
+
+
+NS_IMAGE_END
+
---
+++ PPImager/PPImager/ImageAlgorithm/Triangle.h
@@ -0,0 +1,24 @@
+//
+//  Triangle.h
+//  PPImager
+//
+//  Created by 邱开禄 on 2020/01/18.
+//  Copyright © 2019 邱开禄. All rights reserved.
+//
+
+#ifndef Triangle_H
+#define Triangle_H
+
+#include <stdio.h>
+#include <vector>
+#include "ImageCommon.h"
+
+NS_IMAGE_BEGIN
+
+class Triangle
+{
+    
+};
+NS_IMAGE_END
+#endif // KMEANS_H
+
